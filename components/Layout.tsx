753e704735d1fa4af4bbfe0f8ad9b5acd24a5e6e
phibersoft <phibersoft@hotmail.com>
2020-10-18 22:32:05 +0300

    Google analytics

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,15 +1,32 @@
-import React, { ReactNode } from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import React, { ReactNode } from "react";
+import Link from "next/link";
+import Head from "next/head";
 
 type Props = {
-  children?: ReactNode
-  title?: string
-}
+  children?: ReactNode;
+  title?: string;
+};
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
+const Layout = ({ children, title = "This is the default title" }: Props) => (
   <div>
     <Head>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=UA-152902992-2"
+      ></script>
+
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-152902992-2');
+  `,
+        }}
+      />
+
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
@@ -18,15 +35,15 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <nav>
         <Link href="/">
           <a>Home</a>
-        </Link>{' '}
-        |{' '}
+        </Link>{" "}
+        |{" "}
         <Link href="/about">
           <a>About</a>
-        </Link>{' '}
-        |{' '}
+        </Link>{" "}
+        |{" "}
         <Link href="/users">
           <a>Users List</a>
-        </Link>{' '}
+        </Link>{" "}
         | <a href="/api/users">Users API</a>
       </nav>
     </header>
@@ -36,6 +53,6 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <span>I'm here to stay (Footer)</span>
     </footer>
   </div>
-)
+);
 
-export default Layout
+export default Layout;
